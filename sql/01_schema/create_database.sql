4e199f5ce7412a5015e4d0cc001aba3e0eb8c2da
Rashad Alaa <rashadalaa2000@gmail.com>
2026-05-04 02:55:19 +0300

    Update create_database.sql

--- sql/01_schema/create_database.sql
+++ sql/01_schema/create_database.sql
@@ -88,21 +88,7 @@ CREATE TABLE dim_drivers (
     active BIT,
     primary_area_id tinyint
 );
----------------------------
--- Build Order Dimension for Fact Tables Integration
--- to resolve ambiguous relationships in Power BI
-CREATE TABLE dim_orders (
-       order_id INT PRIMARY KEY
-      ,order_date DATE NOT NULL
-      ,retailer_id INT NOT NULL
-      ,supplier_id INT NOT NULL
-      ,area_id INT NOT NULL
-      ,driver_id INT NOT NULL
-      ,retailer_segment VARCHAR(50) NOT NULL
-      ,order_status VARCHAR(25) NOT NULL
-      ,gmv DECIMAL(18,2) NOT NULL
-)
----------------------------
+
 -- ==============================================================================
 -- 2. FACT TABLES
 -- ==============================================================================
