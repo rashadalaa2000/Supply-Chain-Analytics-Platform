b52a138902f5fd283aa8444c030b454a239bd097
Rashad Alaa <rashadalaa2000@gmail.com>
2026-05-03 11:34:31 +0300

    add Order_to_Cash_Performance query

--- sql/03_analysis/eda.sql
+++ sql/03_analysis/eda.sql
@@ -9,3 +9,19 @@ SELECT
     (SELECT ROUND(AVG(line_total), 2) FROM fact_order_details) AS avg_order_value,
     (SELECT ROUND(SUM(amount), 2) FROM fact_payments WHERE payment_status IN ('Paid', 'Completed')) AS total_collected,
     (SELECT ROUND(SUM(amount), 2) FROM fact_payments WHERE payment_status NOT IN ('Paid', 'Completed')) AS total_outstanding;
+
+
+-- Order_to_Cash_Performance
+SELECT 
+    SUM(CASE WHEN payment_status = 'Paid' AND delivery_status = 'Delivered' THEN line_total ELSE 0 END) AS Realized_Revenue,
+    -- (Net Revenue)
+    
+    SUM(CASE WHEN payment_status = 'Failed' AND delivery_status = 'Delivered' THEN line_total ELSE 0 END) AS Revenue_Loss_Risk,
+    -- (Bad Debt / Risk)
+    
+    SUM(CASE WHEN payment_status = 'Pending' AND delivery_status = 'Delivered' THEN line_total ELSE 0 END) AS Accrued_Revenue
+    -- (Pending Pipeline)
+    
+FROM fact_order_details f
+JOIN fact_payments p ON f.order_id = p.order_id
+JOIN fact_deliveries d ON d.order_id = p.order_id;
