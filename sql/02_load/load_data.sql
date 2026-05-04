ca14244d9701ac1e8820b9e0cc6c6ba06359d6ea
Rashad Alaa <rashadalaa2000@gmail.com>
2026-05-04 07:59:23 +0300

    Fix payment status typo in fact_payments table
    Update payment status to 'Refunded' for incorrect entries.

--- sql/02_load/load_data.sql
+++ sql/02_load/load_data.sql
@@ -107,3 +107,9 @@ SELECT
     extra_delay_days
 FROM supply_chain.dbo.source_deliveries d
 JOIN supply_chain.dbo.source_orders o ON d.order_id = o.order_id;
+
+
+-- Transaction
+UPDATE fact_payments
+SET payment_status = 'Refunded'
+WHERE payment_status = 'Refunde';
